5196b397cca5d69d3167511daae844e36d9bfff6
edmond <edmond.berne6@gmail.com>
2024-11-04 15:36:30 +0100

    update convertir chr38-39

--- multi_request.py
+++ multi_request.py
@@ -3,7 +3,6 @@ from  pyensembl import EnsemblRelease
 from multiprocessing import Pool
 import time
 import json
-import pandas as pd
 
 GENE_PER_PROCESS = 5
 NB_PROCESS = 16
@@ -27,34 +26,47 @@ def request_gene(parameters : tuple):
         # Vérifier le statut de la réponse
         if response.status_code == 200:
             sequence = json.loads(response.text) # convertit la str en dictionnaire
-            liste_start, liste_end, chr, 
-            for match in sequence["mappings"]:
+            liste_start, liste_end, chr, strand = [], [], "", ""
+            for match in sequence["mappings"]: # browse all the matches
+                start_gen = match["start"] # get the start and the end position
+                end_gen = match["end"]
+                if chr == "" and strand == "":
+                    chr = match["seq_region_name"]
+                    strand = match["strand"]
 
-            start_gen = sequence["mappings"][0]["start"]
-            start_arn = transcript_dict[transcript_id][0]
-            end_gen = sequence["mappings"][0]["end"]
-            end_arn = transcript_dict[transcript_id][1]
+                new_start, new_end = ConvertCoordAssembly(start_gen, end_gen, specie, chr, strand)
+                liste_start.append(new_start)
+                liste_end.append(new_end)
+
+            dict_sequences[transcrit_id] = {"start" : liste_start, "end" : liste_end}
             # TODO Voir pour diviser le cas où on a plusieurs fois le même transcrit
             # dict_sequences[transcrit_id] = response.text.strip()
         else:
             print(f"Failed to retrieve transcript ID: {response.status_code}")
-            dict_sequences[transcrit_id] = None
+            dict_sequences[transcrit_id] = {"start" : None, "end" : None}
     return dict_sequences
 
-def ConvertCoordAssembly(start : int, end : int, specy : str, chromosome : int, strand : str, assembly1 : str  = "GRCm39", assembly2 : str = "CRCm38"):
+def ConvertCoordAssembly(start : int, end : int, specy : str, chromosome : int, strand : str, assembly1 : str  = "GRCm39", assembly2 : str = "GRCm38"):
     """
     Function to convert dna coordinate from one genome assmbly to another one
     """
     server = "https://rest.ensembl.org"
     ext = f"/map/{specy}/{assembly1}/{chromosome}:{start}..{end}:{strand}/{assembly2}?"
- 
     r = requests.get(server+ext, headers={ "Content-Type" : "application/json"})
     
     if r.status_code == 200:
         decoded = r.json()
-        print(repr(decoded))
+        if decoded["mappings"] != []:
+            update_start = decoded["mappings"][0]["mapped"]["start"]
+            update_end = decoded["mappings"][0]["mapped"]["end"]
+        else:
+            update_start = None
+            update_end = None
+        return update_start, update_end
+
     else :
         print(f"Error cannot convert from {assembly1} to {assembly2}")
+        return None, None
         
 
 def get_dna_sequences(transcript_ids_list : list[str] , start_trans :list[int], end_trans : list[int], specy : str = "mouse"):
@@ -73,41 +85,40 @@ def get_dna_sequences(transcript_ids_list : list[str] , start_trans :list[int], 
         final_dict = {**final_dict, **dict_genes}
     return final_dict
 
+if __name__ == "__main__":
+    # Exemple d'utilisation
+    transcript_ids_list = [
+        "ENSMUST00000193812", "ENSMUST00000193813", "ENSMUST00000193814", "ENSMUST00000193815", "ENSMUST00000193816",
+        "ENSMUST00000193817", "ENSMUST00000193818", "ENSMUST00000193819", "ENSMUST00000193820", "ENSMUST00000193821",
+        "ENSMUST00000193822", "ENSMUST00000193823", "ENSMUST00000193824", "ENSMUST00000193825", "ENSMUST00000193826",
+        "ENSMUST00000193827", "ENSMUST00000193828", "ENSMUST00000193829", "ENSMUST00000193830", "ENSMUST00000193831"
+    ]
 
-# Exemple d'utilisation
-transcript_ids_list = [
-    "ENSMUST00000193812", "ENSMUST00000193813", "ENSMUST00000193814", "ENSMUST00000193815", "ENSMUST00000193816",
-    "ENSMUST00000193817", "ENSMUST00000193818", "ENSMUST00000193819", "ENSMUST00000193820", "ENSMUST00000193821",
-    "ENSMUST00000193822", "ENSMUST00000193823", "ENSMUST00000193824", "ENSMUST00000193825", "ENSMUST00000193826",
-    "ENSMUST00000193827", "ENSMUST00000193828", "ENSMUST00000193829", "ENSMUST00000193830", "ENSMUST00000193831"
-]
+    start_trans = [
+        1, 1, 1, 1, 1,
+        1, 1, 1, 1, 1,
+        1, 1, 1, 1, 1,
+        1, 1, 1, 1, 1
+    ]
 
-start_trans = [
-    1, 1, 1, 1, 1,
-    1, 1, 1, 1, 1,
-    1, 1, 1, 1, 1,
-    1, 1, 1, 1, 1
-]
+    end_trans = [
+        1000, 1100, 1200, 1300, 1400,
+        1500, 1600, 1700, 1800, 1900,
+        2000, 2100, 2200, 2300, 2400,
+        2500, 2600, 2700, 2800, 2900
+    ]
+    transcript_dict = {transcript_id: (start, end) for transcript_id, start, end in zip(transcript_ids_list, start_trans, end_trans)}
 
-end_trans = [
-    1000, 1100, 1200, 1300, 1400,
-    1500, 1600, 1700, 1800, 1900,
-    2000, 2100, 2200, 2300, 2400,
-    2500, 2600, 2700, 2800, 2900
-]
-transcript_dict = {transcript_id: (start, end) for transcript_id, start, end in zip(transcript_ids_list, start_trans, end_trans)}
+    # Appeler la fonction get_dna_sequences
+    sequences = get_dna_sequences(transcript_ids_list, start_trans, end_trans, specy="mouse")
 
-# Appeler la fonction get_dna_sequences
-sequences = get_dna_sequences(transcript_ids_list, start_trans, end_trans, specy="mouse")
+    # Afficher les séquences récupérées
+    for transcript_id, dict_transcript in sequences.items():
+        liste_start = dict_transcript["start"]
+        liste_end = dict_transcript["end"]
+        if liste_start != None or liste_end != None:
 
-# Afficher les séquences récupérées
-for transcript_id, sequence in sequences.items():
-    if sequence != None :
-        sequence = json.loads(sequence) # convertit la str en dictionnaire
-        start_gen = sequence["mappings"][0]["start"]
-        start_arn = transcript_dict[transcript_id][0]
-        end_gen = sequence["mappings"][0]["end"]
-        end_arn = transcript_dict[transcript_id][1]
-        print(f"Transcript ID: {transcript_id} position {start_arn} : {end_arn}, Genomic position: {start_gen} : {end_gen}")  # Afficher les 100 premiers nucléotides
-    else:
-        print(f"Transcript ID: {transcript_id} not found")
+            print(f"Le transcrit {transcript_id} avec les coordonnées {liste_start} - {liste_end} ")
+
+        else:
+            print(f"Transcript ID: {transcript_id} not found")
