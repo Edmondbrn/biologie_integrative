caf0228c6b2444993f155d715509d8433a3d09dd
edmond <edmond.berne6@gmail.com>
2024-11-25 14:27:04 +0100

    maj filter

--- parsing_rmats.py
+++ parsing_rmats.py
@@ -38,31 +38,33 @@ def filterStrand(rmats_list):
     return dict_file
 
 
-def filterA5SS(data : pd.DataFrame, splice_type : str):
+def filterA5SS(data : pd.DataFrame, splice_type : str) -> None:
     data = data[["GeneID", "chr", "strand", "shortEE", "longExonEnd", "flankingES"]]
     data.columns = ["GeneID", "chr", "strand", "shortSplice", "longSplice", "shareSplice"]
     data.to_csv(f"filteredRmats/{splice_type}_filtered.csv", sep = "\t")
 
-def filterA3SS(data, splice_type):
+def filterA3SS(data, splice_type) -> None:
     data = data[["GeneID", "chr", "strand", "shortES", "longExonStart_0base", "flankingEE"]]
     data.columns = ["GeneID", "chr", "strand", "shortSplice", "longSplice", "shareSplice"]
     data.to_csv(f"filteredRmats/{splice_type}_filtered.csv", sep = "\t")
 
-def filterRI(data):
-    # Implémentez la logique de filtrage pour RI
-    pass
+def filterRI(data : pd.DataFrame, splice_type : str) -> None:
+    data = data[["GeneID", "chr", "strand", "riExonStart_0base", "riExonEnd"]]
+    data.columns = ["GeneID", "chr", "strand", "RiStart", "RiEnd"]
+    data.to_csv(f"filteredRmats/{splice_type}_filtered.csv", sep = "\t")
 
-def filterSE(data):
+def filterSE(data : pd.DataFrame, splice_type : str):
     # Implémentez la logique de filtrage pour SE
     pass
 
-def filterMXEPlus(data):
+def filterMXE(data : pd.DataFrame, splice_type : str) -> None:
     # Implémentez la logique de filtrage pour MXE+
+    data = data[["GeneID", "chr", "strand", "1stExonStart_0base", "1stExonEnd", "2ndExonStart_0base", "2ndExonEnd", "upstreamEE", "downstreamES"]]
+    data.columns = ["GeneID", "chr", "strand", "1stExonStart", "1stExonEnd", "2ndExonStart", "2ndExonEnd", "upstreamEE", "downstreamES"]
+    data.to_csv(f"filteredRmats/_filtered.csv", sep = "\t")
     pass
 
-def filterMXENeg(data):
-    # Implémentez la logique de filtrage pour MXE-
-    pass
+
 
 def chooseParsing(rmats_dict):
     """
@@ -72,13 +74,13 @@ def chooseParsing(rmats_dict):
         "A5SS_+": filterA5SS,
         "A5SS_-": filterA3SS,
         "A3SS_+": filterA3SS,
-        "A3SS_-": filterA5SS #,
-        # "RI_+": filterRI,
-        # "RI_-": filterRI,
+        "A3SS_-": filterA5SS,
+        "RI_+": filterRI,
+        "RI_-": filterRI,
+        "MXE_+": filterMXE,
+        "MXE_-": filterMXE #,
         # "SE_+": filterSE,
         # "SE_-": filterSE,
-        # "MXE_+": filterMXEPlus,
-        # "MXE_-": filterMXENeg,
     }
     if not os.path.isdir("filteredRmats"):
         os.mkdir("filteredRmats")
@@ -91,6 +93,7 @@ def chooseParsing(rmats_dict):
 
 
 if __name__ == "__main__":
-    liste_imp = getRmatsFiles("/home/edmond/Documents/GB5/biologie_integrative/rmats_post")
+    liste_imp = getRmatsFiles("rmats_post")
     rmats_dico = filterStrand(liste_imp)
     chooseParsing(rmats_dico)
+
