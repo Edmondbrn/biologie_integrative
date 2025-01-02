2c89ce20af0918163014fbfa90d3e29ae623866c
edmond <edmond.berne6@gmail.com>
2025-01-02 14:06:02 +0100

    ajout des menus pour les différentes distances

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import sys
 from PyQt6.QtWidgets import (
     QMainWindow, QApplication, QVBoxLayout, QWidget, QPushButton,
-    QLabel, QToolBar, QStatusBar, QCheckBox
+    QLabel, QToolBar, QStatusBar, QCheckBox, QMenu
 )
 from PyQt6.QtGui import QAction, QIcon
 from PyQt6.QtCore import Qt
@@ -54,30 +54,51 @@ class MainWindow(QMainWindow):
         button_convert.setStatusTip("Convert RNA coordinates to genomic coordinates")
         button_convert.triggered.connect(lambda: self.onMyToolBarButtonClick("test pression bouton"))
 
-        # Bouton pour calculer les distances entre les coordonnées
-        button_distances = QAction(QIcon(f"{ICON_PATH}ruler.png"), "Calculate distances", self)
-        button_distances.setStatusTip("Calculate distances between coordinates")
-        button_distances.triggered.connect(lambda: self.onMyToolBarButtonClick("test pression bouton"))
+ 
+        # ============================ définition des sous menus ===============================
+        calculate_distances_menu = QMenu("Calculate distances", self)
+        calculate_distances_menu.setIcon(QIcon(f"{ICON_PATH}ruler.png"))
+        # Ajoute les actions pour les différents types d'épissage alternatif
+        for splice_type in [ "A5SS", "A3SS", "RI", "MXE", "SE"]:
+            action = QAction(f"Calculate distances for {splice_type}", self)
+            action.setStatusTip(f"Calculate distances for {splice_type} events")
+            action.triggered.connect(lambda checked, st=splice_type: self.onCalculateDistances(st))
+            calculate_distances_menu.addAction(action)
 
+        # Ajoute les options all_splicing et manual
+        action_all_splicing = QAction("Calculate distances for all splicing", self)
+        action_all_splicing.triggered.connect(lambda: self.onCalculateDistances("all_splicing"))
+        action_all_splicing.setStatusTip("Calculate distances for all splicing events")
+        calculate_distances_menu.addAction(action_all_splicing)
+
+        action_manual = QAction("Manual calculation", self)
+        action_manual.triggered.connect(lambda: self.onCalculateDistances("manual"))
+        action_manual.setStatusTip("Calculate distances manually")
+        calculate_distances_menu.addAction(action_manual)
 
 
         self.setStatusBar(QStatusBar(self))
 
         menu = self.menuBar()
+        # menu File de la barre de tâches
         file_menu = menu.addMenu("File")
         file_menu.addAction(button_load_data)
         file_menu.addAction(buttton_save_data)
         file_menu.addAction(button_quit)
-
+        # Menu Action de la barre de tâches
         action_menu = menu.addMenu("Actions")
         action_menu.addAction(button_convert)
-        action_menu.addAction(button_distances)
+        action_menu.addMenu(calculate_distances_menu)
+
 
 
 
 
     def onMyToolBarButtonClick(self, s):
         print("Button clicked", s)
+    def onCalculateDistances(self, splice_type):
+        print(f"Calculating distances for {splice_type}")
+
 
 def main():
     app = QApplication(sys.argv)
