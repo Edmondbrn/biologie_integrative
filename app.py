092bddd4a2ab3a97ae7bdc56391f587daba3b6ee
Exovie <francois.martin27@etu.univ-cotedazur.fr>
2025-01-03 16:02:29 +0100

    menus dynamiques pour l'imports de fichiers et import, suppression et visualisation des fichiers rmats

--- app.py
+++ app.py
@@ -1,12 +1,15 @@
 import sys
 from PyQt6.QtWidgets import (
     QMainWindow, QApplication, QVBoxLayout, QWidget, QPushButton,
-    QLabel, QToolBar, QStatusBar, QCheckBox, QMenu, QSpacerItem, QSizePolicy
+    QLabel, QToolBar, QStatusBar, QCheckBox, QMenu, QSpacerItem, QSizePolicy,
+    QFileDialog, QToolButton, QTableWidgetItem, QTableWidget, 
 )
 from PyQt6.QtGui import QAction, QIcon
 from PyQt6.QtCore import Qt, QSize
 from app_utils import FileDialogManual
 from GLOBAL import *
+
+from distances import *
 
 ICON_PATH = "Ressources/Icones/fugue-icons-3.5.6/icons/"
 
@@ -17,6 +20,10 @@ def load_stylesheet(file_path):
 class MainWindow(QMainWindow):
     def __init__(self) -> None:
         super().__init__()
+
+        self.file_path = None
+        self.prot_file = None
+        self.genomic_file = None
 
         self.setWindowTitle("BI Project")
         self.setWindowIcon(QIcon(f"{ICON_PATH}BI_logo.png"))
@@ -48,18 +55,17 @@ class MainWindow(QMainWindow):
         spacer = QSpacerItem(20, 100, QSizePolicy.Policy.Minimum, QSizePolicy.Policy.Minimum)
         layout.addItem(spacer)
 
-
-
     def __create_menu(self):
         # On créé une barre d'outils
         toolbar = QToolBar("My main toolbar")
+        toolbar.setObjectName("My main toolbar")
         self.addToolBar(toolbar)
 
         # ============================ définition des boutons ===============================
         # bouton pour charger un fichier
         button_load_data = QAction(QIcon(f"{ICON_PATH}document-excel.png"), "Load data", self)
         button_load_data.setStatusTip("Load data from a CSV table")
-        button_load_data.triggered.connect(lambda: self.onMyToolBarButtonClick("test pression bouton"))
+        button_load_data.triggered.connect(self.open_file_dialog)
         toolbar.addAction(button_load_data)
 
         # Bouton pour sauvegarder les données
@@ -124,6 +130,116 @@ class MainWindow(QMainWindow):
         print("Button clicked", s)
     def onCalculateDistances(self, splice_type):
         print(f"Calculating distances for {splice_type}")
+
+    def open_file_dialog(self):
+        file_path, _ = QFileDialog.getOpenFileName(self, "File Explorer", "", "All Files (*);;CSV Files (*.csv)")
+        if file_path:
+            self.file_path = file_path
+            self.file_loader()
+
+    def save_file_dialog(self):
+        file_name, _ = QFileDialog.getSaveFileName(self, "File Explorer", "", "All Files (*);;CSV Files (*.csv)")
+        if file_name:
+            self.file_name = file_name
+
+    def file_loader(self):
+        if self.file_path:
+            file_object = pd.read_csv(self.file_path, sep="\t")
+            verification_column = 'gene_name'
+            if verification_column in file_object.columns:
+                self.prot_file = file_object
+            else:
+                self.genomic_file = file_object
+            self.dynamic_menues(self.findChild(QToolBar, "My main toolbar"))
+
+    def close_file_custom(self, variable_name: str):
+        setattr(self, variable_name, None)
+        self.dynamic_menues(self.findChild(QToolBar, "My main toolbar"))
+
+
+    def dynamic_menues(self, toolbar):
+        if self.prot_file is not None: #TODO n'arrive pas à lire les fichiers pour FMRP
+            tool_button = QToolButton(self)
+            tool_button.setText("Fichier protéine  ")
+            tool_button.setObjectName("toolButtonProtein")
+            tool_button.setPopupMode(QToolButton.ToolButtonPopupMode.InstantPopup)  # Le menu apparaît immédiatement
+
+            # Créer un menu pour le QToolButton
+            menu_protein = QMenu("Menu protéine", self)
+
+            # Ajouter des actions au menu
+            delete_prot = QAction("Supprimer fichier protéine", self)
+            delete_prot.setStatusTip("Supprimer le fichier protéine")
+            delete_prot.triggered.connect(lambda: self.close_file_custom("prot_file"))
+            menu_protein.addAction(delete_prot)
+
+            # Associer le menu au QToolButton
+            tool_button.setMenu(menu_protein)
+
+            # Ajouter le QToolButton à la barre d'outils
+            toolbar.addWidget(tool_button)
+
+        if self.genomic_file is not None:
+            tool_button_genomic = QToolButton(self)
+            tool_button_genomic.setText("Fichier génomique  ")
+            tool_button_genomic.setObjectName("toolButtonGenomic")
+            tool_button_genomic.setPopupMode(QToolButton.ToolButtonPopupMode.InstantPopup)  # Le menu apparaît immédiatement
+
+            # Créer un menu pour le QToolButton
+            menu_genomic = QMenu("Fichier génomique", self)
+            menu_genomic.setObjectName("Fichier génomique")
+            print(self.genomic_file.head())
+            view_genomic_button = QAction("Visualiser le fichier", self)
+            view_genomic_button.setStatusTip("Visualiser le fichier génomique")
+            view_genomic_button.triggered.connect(lambda: self.csv_viewer(self.genomic_file))
+            menu_genomic.addAction(view_genomic_button)
+
+            # Ajouter des actions au menu
+            delete_genomic = QAction("Supprimer fichier génomique", self)
+            delete_genomic.setStatusTip("Supprimer le fichier génomique")
+            delete_genomic.triggered.connect(lambda: self.close_file_custom("genomic_file"))
+            menu_genomic.addAction(delete_genomic)
+
+            # Associer le menu au QToolButton
+            tool_button_genomic.setMenu(menu_genomic)
+
+            # Ajouter le QToolButton à la barre d'outils
+            toolbar.addWidget(tool_button_genomic)
+
+        elif self.genomic_file is None and self.findChild(QMenu, "Fichier génomique") != None:
+            self.findChild(QToolButton, "toolButtonGenomic").deleteLater()
+        
+        elif self.prot_file is None and self.findChild(QMenu, "Fichier protéine") != None:
+            self.findChild(QToolButton, "toolButtonProtein").deleteLater()
+
+    def csv_viewer(self, file_name):
+        self.viewer = CSVViewer(file_name)
+        self.viewer.show()
+
+class CSVViewer(QWidget):
+    def __init__(self, file_object):
+        super().__init__()
+        self.setWindowIcon(QIcon(f"{ICON_PATH}BI_logo.png"))
+        self.setWindowTitle("CSV Viewer")
+        self.setGeometry(100, 100, 800, 600)
+
+        layout = QVBoxLayout(self)
+        self.tableWidget = QTableWidget()
+        layout.addWidget(self.tableWidget)
+
+        data = file_object.values.tolist()
+        headers = file_object.columns.tolist()
+        if headers[0] == "Unnamed: 0":
+            headers = headers[1:]
+
+        # Remplir la table avec les données du CSV
+        self.tableWidget.setRowCount(len(data))
+        self.tableWidget.setColumnCount(len(data[0])-1)
+        self.tableWidget.setHorizontalHeaderLabels(headers)
+
+        for rowIdx, row in enumerate(data):
+            for colIdx, cell in enumerate(row[1:]):
+                self.tableWidget.setItem(rowIdx, colIdx, QTableWidgetItem(str(cell))) 
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
