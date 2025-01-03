9a3eee86b8daadbc4e1005f5dbd2ae69de01399c
edmond <edmond.berne6@gmail.com>
2025-01-03 18:37:10 +0100

    ajout d'un fichier pour visualiser les positions de FMRP et les positions d'épissage alternatif

--- test.py
+++
@@ -1,9 +0,0 @@
-import os
-import subprocess
-os.chdir(os.path.dirname(__file__))
-print(os.listdir())
-file= "test.R"
-Rscript = "C:\\Program Files\\R\\R-4.4.0\\bin\\Rscript.exe"
-if os.path.isfile(file):
-    res = subprocess.run([Rscript,file], capture_output=True, text=True)
-    print(res.stdout)
