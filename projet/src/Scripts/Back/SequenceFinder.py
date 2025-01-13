43f50ca5dc79aa2ead7cb5ebd61a2ae1764925f6
edmond <edmond.berne6@gmail.com>
2025-01-13 11:31:20 +0100

    coorection conversion id en python

--- projet/src/Scripts/Back/SequenceFinder.py
+++ projet/src/Scripts/Back/SequenceFinder.py
@@ -2,9 +2,10 @@ import pyensembl as pb
 from pandas import read_csv, DataFrame
 import os
 import regex
+import ast
 
 NB_PROCESS = 4
-ENSEMBL_NAME = "ensembl_transcript_id"
+ENSEMBL_NAME = "ensembl_id"
 SEQUENCE_NAME = "seq"
 RELEASE = 102
 
@@ -30,6 +31,10 @@ class SequenceFinder():
         """
         # self.__data_splice = data_splice
         self.__data_prot = data_prot
+        litteral_col = []
+        for row in self.__data_prot["ensembl_id"]:
+            litteral_col.append(ast.literal_eval(row))
+        self.__data_prot["ensembl_id"] = litteral_col
         self.__species = species
         self.__assembly = aim_assembly
         # TODO change the ensemble release according to user input
@@ -83,7 +88,8 @@ class SequenceFinder():
             for j in range(i * number_id_per_dict, (i + 1) * number_id_per_dict): # filling the dictionnaries
                 if j >= size_ensembl_id: # if we reach the end of the list of ensembl ID (avoid out of range if the division is not absolute)
                     break
-                dict_tmp[(ensembl_id[j], j)] = fixation_sequence[j]
+
+                dict_tmp[(tuple(ensembl_id[j]), j)] = fixation_sequence[j]
                 # On a un dictionnaire avec en clé les identifiants Ensembl et en valeur les séquences de fixation
             self.__general_list.append(dict_tmp)
         return None
@@ -110,28 +116,36 @@ class SequenceFinder():
         end = start + len(sequence)
         return start, end
 
-    def __alignSequences(self, parameters : list[pb.Database , dict[ tuple[str, int] : str]]) -> dict[ tuple[str, int] : tuple[int, int]]:
+    def __alignSequences(self, parameters: list[pb.Database, dict[tuple[str, int], str]]) -> dict[tuple[str, int], tuple[int, int]]:
         """
         Method to align the sequences on the cDNA from pyensembl
         """
         bdd, input_data = parameters
-        ensembl_id = input_data.keys()
+        ensembl_ids = input_data.keys()
         sequences = input_data.values()
         result = {}
-        for ensembl_id, sequence in zip(ensembl_id, sequences):
-            try:
-                transcript : pb.Transcript = bdd.transcript_by_id(ensembl_id[0])
+        
+        for ensembl_id, sequence in zip(ensembl_ids, sequences):
+            transcript = None
+            for ensembl_id_part in ensembl_id[0]:
+                try:
+                    transcript = bdd.transcript_by_id(ensembl_id_part)
+                    break  # Si on trouve un transcript valide, on sort de la boucle
+                except:
+                    continue  # Si on ne trouve pas, on continue avec le prochain identifiant
+            
+            if transcript is None:
+                result[ensembl_id] = ("unknown", "unknown"), [("unknown", "unknown")], 'unknown'
+            else:
                 cDNA = transcript.sequence
                 gene = transcript.gene_id
-            except:
-                result[ensembl_id] = ("unknown", "unknown"), [("unknown", "unknown")], 'unknown'
-            else :
                 rna_start, rna_end = SequenceFinder.align(cDNA, sequence)
                 if SequenceFinder.isRnaCoordNumber(rna_start) and SequenceFinder.isRnaCoordNumber(rna_end):
-                    self.genomic_coordinate_list = self.__spliced_to_genomic(transcript, range(rna_start, rna_end+1))
+                    self.genomic_coordinate_list = self.__spliced_to_genomic(transcript, range(rna_start, rna_end + 1))
                     result[ensembl_id] = ((rna_start, rna_end), self.genomic_coordinate_list, gene)
                 else:
                     result[ensembl_id] = ("Not found", "Not found"), [("Not found", "Not found")], "Not found"
+        
         return result
     
     def __spliced_to_genomic(self, transcript : pb.Transcript, spliced_positions : list[int]):
@@ -192,17 +206,14 @@ class SequenceFinder():
         """
         start_list = {"start_ensembl" : list()}
         end_list = {"end_ensembl" : list()}
-        list_id = {"ensembl_id" : list()}
         gene_list_id = {"GeneID" : list()}
         for id, coord in coord_dict.items():
             start_list["start_ensembl"].append(coord[0][0])
             end_list["end_ensembl"].append(coord[0][1])
-            list_id["ensembl_id"].append(id[0])
             gene_list_id["GeneID"].append(coord[2])
 
         self.__data_prot = self.__data_prot.join(DataFrame(start_list))
         self.__data_prot = self.__data_prot.join(DataFrame(end_list))
-        self.__data_prot = self.__data_prot.join(DataFrame(list_id))
         self.__data_prot = self.__data_prot.join(DataFrame(gene_list_id))
         return None
     
@@ -243,7 +254,7 @@ class SequenceFinder():
             end_list["end_genomic"].append(self.__data_prot.iloc[i]["end_genomic_complete"][-1])
         self.__data_prot = self.__data_prot.join(DataFrame(start_list))
         self.__data_prot = self.__data_prot.join(DataFrame(end_list))
-        self.__data_prot.to_csv("data_filteredfinal.tsv", sep = "\t", index = False)
+        self.__data_prot.to_csv("data_filteredfinal2.tsv", sep = "\t", index = False)
 
         
      
@@ -253,6 +264,6 @@ class SequenceFinder():
 
 
 if __name__ == "__main__":
-    df_prot = read_csv("data_filtered.tsv", sep = "\t", header = 0)
+    df_prot = read_csv("/home/edmond/Documents/GB5/biologie_integrative/projet/src/Ressources/data/FMRP_Binding_sites_mouse_Maurin_NAR_2014_merged.tsv_converted", sep = "\t", header = 0)
     app = SequenceFinder(df_prot)
     app.start()
